89696e37d24e713e07d1021578a2f9a0dba8732b
delaroystudios <RepubliC7891>
2020-01-10 14:47:13 +0100

    Endpoint for creating and authenticating users

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,16 +1,17 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+
 class ModelTests(TestCase):
     """docstring for ModelTests."""
 
     def test_create_user_with_email_successful(self):
         """Test creating a new user with an email is successful"""
-        email = 'test@mail.com'
-        password = 'Testpass123'
-        user = get_user_model().objects.create_user(
-            email = email,
-            password = password
+        email= 'test@mail.com'
+        password= 'Testpass123'
+        user= get_user_model().objects.create_user(
+            email= email,
+            password= password
         )
 
         self.assertEqual(user.email, email)
